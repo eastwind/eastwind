f4d30d6b567d17fdfd4d234286777fd306b113d1
Lucas Peng <lucasp0927>
2010-03-28 17:18:15 +0000

    add backupandrecover.py which contain backupfile() and recoverfile()
    git-svn-id: https://eastwind.googlecode.com/svn/trunk@6 4b58affa-0d3d-0ee7-3aa6-5fd2a1179c05

---
+++ backupandrecover.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def backupfile(file):
+    """
+    Copy file ( if it is a file copy it, if it is a directory tar it).
+    Save the original path in a seperate log.
+    """
+
+def recoverfile(file):
+    """
+    look up file is log and copy it to that path.
+    """
