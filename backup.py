c0ac648f8f8a3c44dc213e6072213ea9ddad28a4
Lucas Peng <lucasp0927>
2010-03-30 08:25:40 +0000

    Implement hash with time variant.
    git-svn-id: https://eastwind.googlecode.com/svn/trunk@14 4b58affa-0d3d-0ee7-3aa6-5fd2a1179c05

--- backup.py
+++ backup.py
@@ -1,21 +1,32 @@
-
+#!/usr/bin/env python
+import os
+import shutil
+import hashlib
+import time 
 
 """
 Backup scripts and setting.
 """
-import os
-import shutil
 
 def make_filename(file):
     """ Hash the filename with time variant. """
+    return hashlib.sha1(file+"%f"%time.time()).hexdigest()
 
+    
 def backup(file, dest):
-    file=os.path.expanduser(file)
     """ Backup the files to destination. """
+    file=os.path.expanduser(file)
     if os.path.exists(file) == False:
         raise RuntimeError("The file you want to backup does not exist!")
+        
+    dest="%s/%s%s"%(os.path.abspath(os.path.expanduser(dest)),make_filename(file),".tar.bz2")
+    print "Start to tar %s to %s" % (file,dest)
     """ Tar the target into a single file """
-    os.system("cd %s && tar -jpcv -f %s ./%s" % (os.path.split(os.path.abspath(file))[0], dest, os.path.split(os.path.abspath(file))[1]))
+    os.system("cd %s && tar -jpc -f %s ./%s" % (os.path.split(os.path.abspath(file))[0], dest, os.path.split(os.path.abspath(file))[1]))
+    print "Backup success"
 
 def recover(file, dest):
     """ look up file is log and copy it to that path. """
+
+backup("~/.emacs.d","~/backup/")
+
