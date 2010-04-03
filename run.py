57ea86ab6e858c20a71452d3af0bd69e4a2dfcec
Stimim Chen <death1048576@gmail.com>
2010-04-03 01:55:50 +0000

    Replace run.py by run_test.py, backup.py by backup_test.py
    git-svn-id: https://eastwind.googlecode.com/svn/trunk@28 4b58affa-0d3d-0ee7-3aa6-5fd2a1179c05

--- run.py
+++ run.py
@@ -22,8 +22,7 @@ class EastWind:
             self.update()
             self.install()
         elif sys.argv[1] == "backup":
-            self.clean_backup()
-            self.load()
+            self.load_backup_list()
             self.backup()
         else:
             print "The argument is not correct!"
@@ -41,28 +40,53 @@ class EastWind:
                     self.install_list.append(t["name"])
                 if "ppa" in t:
                     self.ppa_list.append(t["ppa"])
-            for t in s.info['Backup']:
-                if "path" in t:
-                    if type(t['path']) == list:
-                        for u in t['path']:
-                            self.backup_list.info['Path'].append({"path": u})
-                    else:
-                        self.backup_list.info['Path'].append({"path": t['path']})
 
-    def clean_backup(self):
-        os.system("rm backup/*.tar.bz2")
-        os.remove("backup.json")
-        self.backup_list = JsonInfo("backup.json")
+    def load_backup_list(self):
+        # TODO The performence of checking blist might not be good enough.
+        """
+        load 'path' from json files, add them to backup list if
+        it's not in the list.
+        """
+        blist = []
+        for v in self.backup_list.info['Path']:
+            blist.append(v['path'])
+
+        for jsons in os.listdir('setting'):
+            if re.match("[^.]*.json$",jsons) == None:
+                continue
+
+            s = JsonInfo( 'setting/%s' % jsons )
+            for t in s.info['Backup']:
+                if 'path' in t:
+                    if type(t['path']) == list:
+                        v = t.copy()
+                        for u in t['path']:
+                            if u not in blist:
+                                print "add %s to backup list" % u
+                                v['path']=u
+                                self.backup_list.info['Path'].append(v.copy())
+                                blist.append(u)
+                    else:
+                        if t['path'] not in blist:
+                            print "add %s to backup list" % t['path']
+                            self.backup_list.info['Path'].append(t)
+                            blist.append(t['path'])
 
     def backup(self):
         """ Backup files """
         print "Start to backup files:"
-        for i in self.backup_list.info['Path']:
+        b = self.backup_list.info['Path']
+        for i in b[:]:
             print "  %s" % i["path"]
-            i["backuped"] = backup(i["path"])
+            tmp = backup(i)
+            #print tmp
+            if tmp != None:
+                b[b.index(i)]=tmp.copy()
+            else:
+                # tmp == None if the i['path'] is not exist.
+                b.remove(i)
         self.backup_list.write()
         """  Remove old backup files. """
-        
 
     def recover(self):
         """ Recover files """
