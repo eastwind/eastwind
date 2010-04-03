65c6b2253e65762c2af4beaa9e9e23fa76539f45
Lucas Peng <lucasp0927>
2010-04-03 07:10:01 +0000

    Adjust layout and add a text-view for expnanation text.
    git-svn-id: https://eastwind.googlecode.com/svn/trunk@30 4b58affa-0d3d-0ee7-3aa6-5fd2a1179c05

--- gui.py
+++ gui.py
@@ -19,17 +19,22 @@ class EastWind:
         
         button = gtk.Button(stock=gtk.STOCK_QUIT)
         button.connect("clicked", lambda w: gtk.main_quit())
-        table.attach(button, 14, 15, 14, 15)
+        button.set_size_request(70,35)
+        table.attach(button, 14, 15, 14, 15,gtk.FILL, 
+            gtk.SHRINK, 1, 1)
         button.show()
 
         button = gtk.Button("GO!")
-        table.attach(button, 13, 14, 14, 15)
+        button.set_size_request(70,35)
+        table.attach(button, 13, 14, 14, 15,gtk.FILL, 
+            gtk.SHRINK, 1, 1)
         button.show()
 
     def make_notebook(self,table):
         notebook = gtk.Notebook()
         notebook.set_tab_pos(gtk.POS_TOP)
-        table.attach(notebook, 0,15,0,14)
+        notebook.set_size_request(400,400)
+        table.attach(notebook, 0,13,0,14)
         notebook.show()
         table.show()
         self.show_tabs = True
@@ -44,6 +49,13 @@ class EastWind:
 
             label = gtk.Label(i)
             notebook.append_page(frame, label)
+            
+    def make_explanation(self,table):
+        wins = gtk.TextView()
+        wins.modify_fg(gtk.STATE_NORMAL, gtk.gdk.Color(5140, 5140, 5140))
+        wins.set_cursor_visible(False)
+        table.attach(wins, 13, 15, 0, 14, gtk.FILL | gtk.EXPAND,gtk.FILL | gtk.EXPAND, 1, 1)
+        wins.show()
         
     def __init__(self):
 
@@ -53,11 +65,14 @@ class EastWind:
         self.window.connect("destroy", self.destroy)
         self.window.set_border_width(10)
 
-        table = gtk.Table(15,15,True)
+        table = gtk.Table(15,15,False)
+        table.set_col_spacings(3)
         self.window.add(table)
+        
         self.make_notebook(table)
         self.make_button(table)
-
+        self.make_explanation(table)
+        
         self.window.show()
         self.show_tabs=True
         
