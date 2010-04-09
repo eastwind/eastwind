c013e6219ec493140e587adb56c06480fae10076
Andrew Liu <andrewliu33@gmail.com>
2010-04-09 12:21:08 +0000

    Combine run.py with eastwind.py.
    git-svn-id: https://eastwind.googlecode.com/svn/trunk@35 4b58affa-0d3d-0ee7-3aa6-5fd2a1179c05

--- eastwind.py
+++ eastwind.py
@@ -27,8 +27,7 @@ class EastWind:
         elif sys.argv[1] == "ppa":
             self.to_ppa(sys.argv[2])
         elif sys.argv[1] == "backup":
-            self.clean_backup()
-            self.load()
+            self.load_backup_list()
             self.backup()
         else:
             print "The argument is not correct!"
@@ -49,7 +48,6 @@ class EastWind:
         s.info['Apps'].append({"ppa": pkg_src})
         s.write()
 
-
     def load(self):
         """ Loads all the settings from ./setting """
         print "Start loading settings:"
@@ -63,32 +61,68 @@ class EastWind:
                     self.install_list.append(t["name"])
                 if "ppa" in t:
                     self.ppa_list.append(t["ppa"])
-            for t in s.info['Backup']:
-                if "path" in t:
-                    if type(t['path']) == list:
-                        for u in t['path']:
-                            self.backup_list.info['Path'].append({"path": u})
-                    else:
-                        self.backup_list.info['Path'].append({"path": t['path']})
 
-    def clean_backup(self):
-        os.system("rm backup/*.tar.bz2")
-        os.remove("backup.json")
-        self.backup_list = JsonInfo("backup.json")
+    def load_backup_list(self):
+        # TODO The performence of checking blist might not be good enough.
+        """
+        load 'path' from json files, add them to backup list if
+        it's not in the list.
+        """
+        # blist record the 'path' which are already in backup_list
+        # when we add a path into backup_list, we also add it into blist
+        blist = []
+        for v in self.backup_list.info['Path']:
+            blist.append(v['path'])
+
+        for jsons in os.listdir('setting'):
+            if re.match("[^.]*.json$",jsons) == None:
+                continue
+
+            s = JsonInfo( 'setting/%s' % jsons )
+            for t in s.info['Backup']:
+                if 'path' in t:
+                    if type(t['path']) == list:
+                        v = t.copy()
+                        for u in t['path']:
+                            if u not in blist:
+                                print "add %s to backup list" % u
+                                v['path']=u
+                                self.backup_list.info['Path'].append(v.copy())
+                                blist.append(u)
+                    else:
+                        if t['path'] not in blist:
+                            print "add %s to backup list" % t['path']
+                            self.backup_list.info['Path'].append(t)
+                            blist.append(t['path'])
 
     def backup(self):
         """ Backup files """
         print "Start to backup files:"
-        for i in self.backup_list.info['Path']:
+        b = self.backup_list.info['Path']
+        # Since we might change backup_list in the loop, we iterate on a
+        # copy of b, which denote by b[:]
+        for i in b[:]:
             print "  %s" % i["path"]
-            i["backuped"] = backup(i["path"])
+            tmp = backup(i)
+            if tmp != None:
+                b[b.index(i)]=tmp.copy()
+            else:
+                # tmp == None if i['path'] is not exist, remove it from
+                # backup list
+                b.remove(i)
         self.backup_list.write()
+        """  Replace old backup list. """
 
     def recover(self):
         """ Recover files """
         for i in self.backup_list.info['Path']:
             if 'backuped' in i:
-                recover(i['backuped'], i['path'])
+                try:
+                    recover(i['backuped'], i['path'])
+                except NotFoundError as e:
+                    print "Can't find file %s" % e
+                except RecoverError as e:
+                    print "Failed to recover %s" % e
 
     def source(self):
         print "Start adding sources:"
