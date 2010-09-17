4cc7e37ddb8988059d7dbddafc94e1f99de5e08d
Lucas <lucasp0927@gmail.com>
2010-09-17 11:35:03 +0800

    add pacman_series.py

---
+++ pkgmanager/pacman_series.py
@@ -0,0 +1,50 @@
+'''
+Eastwind Package Management Interface APT Implementation
+'''
+
+import subprocess
+
+import utils
+from manager_base import EastwindPkgMangerSkeleton
+
+class EastwindPkgMangerAPT(EastwindPkgMangerSkeleton):
+    def update(self):
+        utils.need_root_access('pacman -Syy')
+        handle = subprocess.Popen('sudo pacman -Syy',
+                                  shell=True)
+
+    def upgrade(self):
+        utils.need_root_access('pacman -Su')
+        handle = subprocess.Popen('sudo pacman -Su',
+                                  shell=True).wait()
+        stdout, stderr = handle.communicate('y\n')
+
+    def install(self, pkgs):
+        utils.need_root_access('pacman -S')
+        handle = subprocess.Popen('sudo pacman -S %s'
+                                  % (" ".join(pkgs)),
+                                  stdin=subprocess.PIPE,
+                                  shell=True)
+        stdout, stderr = handle.communicate('y\n')
+
+    def install_interactive(self, pkgs):
+        utils.need_root_access('pacman -S')
+        handle = subprocess.Popen('sudo pacman -S %s' % (" ".join(pkgs)),
+                                  shell=True).wait()
+
+    def purge(self, pkgs):
+        utils.need_root_access('pacman -Rns')
+        handle = subprocess.Popen('sudo pacman -Rsn --purge %s'
+                                  % (" ".join(pkgs)),
+                                  stdin=subprocess.PIPE,
+                                  shell=True)
+        stdout, stderr = handle.communicate('y\n')
+
+#    def add_external_sources(self, sources):
+#        utils.need_root_access('add-apt-repository')
+#        for source in sources:
+#            print 'Adding %s to system...' % source
+#            handle = subprocess.Popen('sudo add-apt-repository %s' % source,
+#                                      shell=True).wait()
+
+#TODO AUR
