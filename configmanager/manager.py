a456293bba9d2ed92ddcc645dc55179ceeb7a83d
Andrew Liu <andrew.liu@cardinalblue.com>
2010-09-19 14:10:55 +0800

    Add auto-generated information for handler to react.

--- configmanager/manager.py
+++ configmanager/manager.py
@@ -13,6 +13,7 @@ class EastwindConfigManager:
         self.backup_path = backup_path
         self.config = os.path.join(self.backup_path, 'backup')
         self.dir_hash = {}
+        self.path_hash = {}
 
     def backup(self, orig_path):
         """ Backup the files to destination. """
@@ -29,16 +30,16 @@ class EastwindConfigManager:
         #TODO: may raise error when orig_path not available
 
         self.dir_hash[hashed_folder] = os.path.dirname(os.path.normpath(orig_path))
+        self.path_hash[orig_path] = hashed_folder
 
-    def recover(self, hashkey):
+    def recover(self, orig_path):
         """ Copy the backed file to that path. """
         with open(self.config, 'r') as f:
-            self.dir_hash = json.load(f)
-
-        #TODO: need to copy back
+            self.dir_hash, self.path_hash = json.load(f)
+            #TODO: recover self.path_hash[orig_path] to self.dir_hash[self.path_hash[orig_path]]
 
     def dump(self):
         """ Dump the hash => path info to a json """
         with open(self.config, "w") as f:
-            json.dump(self.dir_hash, f, sort_keys=True, indent=4)
+            json.dump([self.dir_hash, self.path_hash], f, sort_keys=True, indent=4)
 
