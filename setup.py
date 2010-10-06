cfc49ae0674240a1afebbbbbb9498d1b3b632293
Andrew Liu <andrewliu33@gmail.com>
2010-10-06 09:51:45 +0800

    Fix the path problem.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from distutils.core import setup
 
 setup(
 	name = 'eastwind',
-	version = '0.1.1',
+	version = '0.1.2',
 	description = 'Tool to make Ubuntu installation easier.',
 	author = 'Andrew Liu',
 	author_email = 'andrewliu33@gmail.com',
         url = 'http://github.com/eastwind/eastwind',
 	license = 'GPL',
-    	packages = ['eastwind'],
+    	packages = ['eastwind', 'eastwind.pkgmanager', 'eastwind.configmanager'],
 	scripts = ['bin/ew'],
         #cmdclass = { "build": build_extra.build_extra,
         #             "build_i18n": build_i18n.build_i18n }
