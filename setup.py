aff5044ff4b987d762ea254fde6a521a3a488033
Andrew Liu <andrewliu33@gmail.com>
2010-10-14 23:37:27 +0800

    eastwind 0.2.0

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ from distutils.core import setup
 
 setup(
 	name = 'eastwind',
-	version = '0.1.2',
+	version = '0.2.0',
 	description = 'Tool to make Ubuntu installation easier.',
 	author = 'Andrew Liu',
 	author_email = 'andrewliu33@gmail.com',
         url = 'http://github.com/eastwind/eastwind',
 	license = 'GPL',
-    	packages = ['eastwind', 'eastwind.pkgmanager', 'eastwind.configmanager'],
+    	packages = ['eastwind', 'eastwind.manager'],
 	scripts = ['bin/ew'],
         #cmdclass = { "build": build_extra.build_extra,
         #             "build_i18n": build_i18n.build_i18n }
 )
+
